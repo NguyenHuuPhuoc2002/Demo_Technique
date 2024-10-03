25b77dddd768b4f978444c2be9d1bce7e66c4663
NguyenHuuPhuoc2002 <nguyenhuuphuocpdl@gmail.com>
2024-10-03 10:27:05 +0700

    serilog

--- Unit_Of_Work/Controllers/StudentsController.cs
+++ Unit_Of_Work/Controllers/StudentsController.cs
@@ -13,30 +13,20 @@ namespace Unit_Of_Work.Controllers
     public class StudentsController : ControllerBase
     {
         private readonly ISinhVienRepository _sinhVien;
-        private readonly IDistributedCache _cache;
-        private string cacheKey = "myCacheKey";
 
         public StudentsController(ISinhVienRepository sinhVien, IDistributedCache cache)
         {
             _sinhVien = sinhVien;
-            _cache = cache;
         }
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            string cacheData = await _cache.GetStringAsync(cacheKey);
-            if(string.IsNullOrEmpty(cacheData))
-            {
                 var students = await _sinhVien.GetAll();
                 cacheData = JsonConvert.SerializeObject(students);
                 await _cache.SetStringAsync(cacheKey, cacheData, new DistributedCacheEntryOptions
                 {
                     AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(30),
                    
-                });
-            }
-            var deserializedStudents = JsonConvert.DeserializeObject<List<SinhVienModel>>(cacheData);
-            return Ok(deserializedStudents);
         }
 
 
@@ -52,7 +42,6 @@ namespace Unit_Of_Work.Controllers
                 NgaySinh = model.NgaySinh
             };
             await _sinhVien.AddItem(result);
-            _cache.Remove(cacheKey);
             return Ok(model);
 
 
@@ -61,7 +50,6 @@ namespace Unit_Of_Work.Controllers
         public async Task<IActionResult> Delete(string id)
         {
             await _sinhVien.RemoveItem(id);
-            _cache.Remove(cacheKey);
             return NoContent();
         }
     }
